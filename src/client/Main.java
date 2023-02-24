eb5130b44dfbcab02717f590138c6d0b09132a5f
Rallve <77719378+Rallve@users.noreply.github.com>
2023-02-24 13:43:46 +0100

    minor update

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,7 +1,7 @@
 package client;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Model m = new Model("10.80.45.106", 1234);
         View v = new View();
         Controller c = new Controller(m, v);
@@ -9,5 +9,8 @@ public class Main {
         ListenerThread l = new ListenerThread(m.in, System.out);
         Thread listener = new Thread(l);
         listener.start();
+        m.runProtocol();
+        listener.join();
+        m.shutDown();
     }
 }
